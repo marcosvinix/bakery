a55f6dbf8d63f673e4070b55216b354dd66a96cf
Marcos Vinicius <marcos1vinicius2@hotmail.com>
2023-01-26 07:36:47 -0300

    updated

--- application/controllers/categorias.php
+++ application/controllers/categorias.php
@@ -13,6 +13,76 @@ class categorias extends CI_Controller {
 		$this->load->view('components/navbar');
 		$this->load->view('components/header', $data);
 		$this->load->view('categorias');
+	
 
+	} 
+
+	public function r($categoria)
+	{
+
+		if($categoria == 'paes'){
+			
+			$data["title"] = "Pães";
+		
+			$this->load->helper('url');
+			$this->load->view('components/navbar');
+			$this->load->view('components/header', $data);
+
+		}
+		if($categoria == 'bolos'){
+			
+			$data["title"] = "Bolos";
+		
+			$this->load->helper('url');
+			$this->load->view('components/navbar');
+			$this->load->view('components/header', $data);
+
+		}
+		if($categoria == 'salgados'){
+			
+			$data["title"] = "Salgados";
+		
+			$this->load->helper('url');
+			$this->load->view('components/navbar');
+			$this->load->view('components/header', $data);
+
+		}
+		if($categoria == 'folhados'){
+			
+			$data["title"] = "Folhados";
+		
+			$this->load->helper('url');
+			$this->load->view('components/navbar');
+			$this->load->view('components/header', $data);
+
+		}
+		if($categoria == 'doces'){
+			
+			$data["title"] = "Docês";
+		
+			$this->load->helper('url');
+			$this->load->view('components/navbar');
+			$this->load->view('components/header', $data);
+
+		}
+		if($categoria == 'bebidas'){
+			
+			$data["title"] = "Bebidas";
+		
+			$this->load->helper('url');
+			$this->load->view('components/navbar');
+			$this->load->view('components/header', $data);
+
+		}
+		if($categoria == 'outros'){
+			
+			$data["title"] = "Outros";
+		
+			$this->load->helper('url');
+			$this->load->view('components/navbar');
+			$this->load->view('components/header', $data);
+
+		}
 	}
+
 }
